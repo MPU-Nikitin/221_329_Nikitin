898cf513e1b133494dfcf5a99ffd0c327f583c9e
Oleg Nikitin <oenikitin@zlp.ooo>
2024-06-25 14:32:01 +0300

    trimmed

--- src/trx.cpp
+++ src/trx.cpp
@@ -5,10 +5,10 @@ Trx::Trx() : sum(""), walletNumber(""), date(""), hash("") {}
 Trx::Trx(const QString &text) {
   QStringList fields = text.split(' ');
   if (fields.size() == 4) {
-    sum = fields[0];
-    walletNumber = fields[1];
-    date = fields[2];
-    hash = fields[3];
+    sum = fields[0].trimmed();
+    walletNumber = fields[1].trimmed();
+    date = fields[2].trimmed();
+    hash = fields[3].trimmed();
   } else {
     sum = "";
     walletNumber = "";
